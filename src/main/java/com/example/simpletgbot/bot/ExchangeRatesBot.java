a9a1b4436a7fd75ba88e649874751ec7c9f57bca
CatOfFluffiness <rabotkalubimaya@gmail.com>
2023-09-10 23:15:25 +0700

    Initial commit

---
+++ src/main/java/com/example/simpletgbot/bot/ExchangeRatesBot.java
@@ -0,0 +1,21 @@
+package com.example.simpletgbot.bot;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+public class ExchangeRatesBot extends TelegramLongPollingBot{
+    public ExchangeRatesBot(@Value("${bot.token}") String botToken) {
+        super(botToken);
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "TgGennady_bot";
+    }
+}
